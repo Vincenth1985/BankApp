1129cc9a3928974186dd2ec7e224fb939fd346d5
Vincent Honca <vincenth1985@gmail.com>
2019-05-09 10:03:17 +0200

    add Interface BankAccountSpecification

--- src/model/BankAccountSpecification.java
+++ src/model/BankAccountSpecification.java
@@ -1,4 +1,15 @@
 package model;
 
 public interface BankAccountSpecification {
+
+
+    public abstract void withdraw();
+
+    public abstract void deposit();
+
+
+    final double MOUNTHLY_INTERESET_RATE = 0;
+    final double MOUNTHLY_INTERESET = 0;
+
+
 }
